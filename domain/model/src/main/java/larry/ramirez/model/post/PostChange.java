494af7cf7fd08dc2d7f4188cf8a40a62217700d0
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-08-03 18:41:07 -0500

    Implemented TODO's

--- domain/model/src/main/java/larry/ramirez/model/post/PostChange.java
+++ domain/model/src/main/java/larry/ramirez/model/post/PostChange.java
@@ -1,12 +1,25 @@
 package larry.ramirez.model.post;
 
+import larry.ramirez.model.post.events.CommentAdded;
+import larry.ramirez.model.post.events.PostCreated;
 import larry.ramirez.model.post.generic.EventChange;
+import larry.ramirez.model.post.values.comment.CommentId;
+import larry.ramirez.model.post.values.post.Title;
+
+import java.util.ArrayList;
 
 public class PostChange extends EventChange {
 
     public PostChange(Post post) {
-        // TODO: post created event logic
+        apply((PostCreated event) -> {
+            post.title = event.getTitle();
+            post.author = event.getPostAuthor();
+            post.commentList = new ArrayList<>();
+        });
 
-        // TODO: comment added event logic
+        apply((CommentAdded event) -> {
+            Comment comment = new Comment(event.getCommentId(), event.getCommentAuthor(), event.getContent());
+            post.commentList.add(comment);
+        });
     }
 }
